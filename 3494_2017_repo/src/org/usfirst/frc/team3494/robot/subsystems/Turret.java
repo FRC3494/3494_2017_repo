a520c286943a22fa08bef802cf5f4d765e77163e
Edwan Vi <tkdberger@gmail.com>
2017-02-09 21:36:18 -0500

    create empty turret sys

---
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/subsystems/Turret.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team3494.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Turret subsystem. Contains methods for controlling the turret.
+ * 
+ * @since 0.0.0
+ */
+public class Turret extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
