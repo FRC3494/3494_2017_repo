8ba149ef7de16e2d2b36d2ce6d636abe029027e0
Edwan Vi <tkdberger@gmail.com>
2017-02-11 15:36:46 -0500

    Source format

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/subsystems/IMotorizedSubsystem.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/subsystems/IMotorizedSubsystem.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3494.robot.subsystems;
 
 /**
  * Interface to apply to subsystems with motors.
+ * 
  * @since 0.0.0
  * @see edu.wpi.first.wpilibj.command.Subsystem
  */
@@ -10,8 +11,10 @@ public interface IMotorizedSubsystem {
 	 * Stop <strong>all</strong> motors on the subsystem.
 	 */
 	public void stopAll();
+
 	/**
-	 * Sets all motors on a subsystem to a given speed. (Example use: driving forward)
+	 * Sets all motors on a subsystem to a given speed. (Example use: driving
+	 * forward)
 	 */
 	public void setAll(double speed);
 }
