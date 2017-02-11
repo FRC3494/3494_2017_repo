ea36f88d0e9264344a15818929ba88b51809a047
Edwan Vi <tkdberger@gmail.com>
2017-02-11 13:14:13 -0500

    Create DriveDirections enum

---
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/DriveDirections.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team3494.robot;
+
+/**
+ * Enum of directions to drive in.
+ * @since 0.0.0
+ */
+public enum DriveDirections {
+	FORWARD, BACKWARD, LEFT, RIGHT, UP, DOWN;
+}
