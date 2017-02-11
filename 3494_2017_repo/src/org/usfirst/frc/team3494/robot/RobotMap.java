7ec005fbee4628396a7322426d553a193aa43c0c
Edwan Vi <tkdberger@gmail.com>
2017-02-11 18:48:18 -0500

    map controllers

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/RobotMap.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/RobotMap.java
@@ -35,4 +35,8 @@ public class RobotMap {
 	public static final int INTAKE_MOTOR = 60;
 	// climber
 	public static final int CLIMBER_MOTOR = 61;
+	// turret
+	public static final int TURRET_RING = 62;
+	public static final int TURRET_LOWER = 63;
+	public static final int TURRET_UPPER = 64;
 }
