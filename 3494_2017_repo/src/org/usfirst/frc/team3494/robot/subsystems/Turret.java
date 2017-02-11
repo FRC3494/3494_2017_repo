e3a85bbd32dc70c4c0f501e1789915f48e4e5b0f
Edwan Vi <tkdberger@gmail.com>
2017-02-11 18:57:11 -0500

    Add remaining motors and encoders to Turret

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/subsystems/Turret.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/subsystems/Turret.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team3494.robot.subsystems;
 
+import org.usfirst.frc.team3494.robot.RobotMap;
+
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -12,7 +16,17 @@ public class Turret extends Subsystem implements IMotorizedSubsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	private Encoder turretRingEnc;
-	private Encoder shooterEnc;
+	private Encoder shooterEnc_lower;
+	private Encoder shooterEnc_upper;
+	private CANTalon shooterUpper;
+	private CANTalon shooterLower;
+	private CANTalon turretRing;
+	public Turret() {
+		super("Turret");
+		this.turretRing = new CANTalon(RobotMap.TURRET_RING);
+		this.shooterUpper = new CANTalon(RobotMap.TURRET_UPPER);
+		this.shooterLower = new CANTalon(RobotMap.TURRET_LOWER);
+	}
 	@Override
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
@@ -21,11 +35,15 @@ public class Turret extends Subsystem implements IMotorizedSubsystem {
 
 	@Override
 	public void stopAll() {
-		
+		this.shooterLower.set(0);
+		this.shooterUpper.set(0);
+		this.turretRing.set(0);
 	}
 
 	@Override
 	public void setAll(double speed) {
-		
+		this.shooterLower.set(speed);
+		this.shooterUpper.set(speed);
+		this.turretRing.set(speed);
 	}
 }
