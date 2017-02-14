9f77e20cd5bcd2563dc27bb65d2f2c446a83e623
Edwan Vi <tkdberger@gmail.com>
2017-02-13 19:49:40 -0500

    Deploy intake command

---
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/intake/Deploy.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team3494.robot.commands.intake;
+
+import org.usfirst.frc.team3494.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * Deploys the intake for the start of teleop.
+ * 
+ * @see org.usfirst.frc.team3494.robot.subsystems.Intake
+ */
+public class Deploy extends Command {
+
+	public Deploy() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.intake);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.intake.pushForward();
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
