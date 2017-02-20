0b1b466c213970b347a9c5b07ad074a34745ee06
Edwan Vi <tkdberger@gmail.com>
2017-02-20 12:40:08 -0500

    javadoc

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/AutoGenerator.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/AutoGenerator.java
@@ -7,15 +7,23 @@ import org.usfirst.frc.team3494.robot.commands.auto.XYDrive;
 
 import edu.wpi.first.wpilibj.command.Command;
 
+/**
+ * Class containing methods that return valid lists to pass to
+ * {@link org.usfirst.frc.team3494.robot.commands.auto.ConstructedAuto}.
+ * 
+ * @since 0.0.3
+ * @see org.usfirst.frc.team3494.robot.commands.auto.ConstructedAuto
+ */
 public class AutoGenerator {
 	public static ArrayList<Command> autoOne() {
 		ArrayList<Command> list = new ArrayList<Command>();
 		list.add(new XYDrive(36, 36));
 		return list;
 	}
+
 	public static ArrayList<Command> crossBaseLine() {
 		ArrayList<Command> list = new ArrayList<Command>();
-		list.add(new DistanceDrive(93.25, UnitTypes.INCHES)); // ffs the baseline is far away
-		return null;
+		list.add(new DistanceDrive(93.25, UnitTypes.INCHES));
+		return list;
 	}
 }
