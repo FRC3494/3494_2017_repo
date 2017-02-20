bb34dd91df8de780a462f9a0880190e4c733ebf3
Edwan Vi <tkdberger@gmail.com>
2017-02-20 13:33:54 -0500

    Tidy up XYDrive

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/XYDrive.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/XYDrive.java
@@ -30,24 +30,7 @@ public class XYDrive extends CommandGroup {
 		double rise2 = rise * rise;
 		this.hypot = Math.sqrt(run2 + rise2);
 		this.angle = Math.toDegrees(Math.atan2(rise, run));
-		requires(Robot.driveTrain);
-		// Add Commands here:
-		// e.g. addSequential(new Command1());
-		// addSequential(new Command2());
-		// these will run in order.
 		addSequential(new AngleTurn(angle));
 		addSequential(new DistanceDrive(hypot, UnitTypes.INCHES));
-
-		// To run multiple commands at the same time,
-		// use addParallel()
-		// e.g. addParallel(new Command1());
-		// addSequential(new Command2());
-		// Command1 and Command2 will run in parallel.
-
-		// A command group will require all of the subsystems that each member
-		// would require.
-		// e.g. if Command1 requires chassis, and Command2 requires arm,
-		// a CommandGroup containing them would require both the chassis and the
-		// arm.
 	}
 }
