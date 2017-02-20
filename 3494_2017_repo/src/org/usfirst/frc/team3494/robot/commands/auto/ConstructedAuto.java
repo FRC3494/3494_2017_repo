6a19274e5360fde8903b55beb01711dd341025d6
Edwan Vi <tkdberger@gmail.com>
2017-02-20 13:35:58 -0500

    Comment cleanup

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/ConstructedAuto.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/ConstructedAuto.java
@@ -19,24 +19,8 @@ public class ConstructedAuto extends CommandGroup {
 	 *            order.</b>
 	 */
 	public ConstructedAuto(ArrayList<Command> commands) {
-		// Add Commands here:
-		// e.g. addSequential(new Command1());
-		// addSequential(new Command2());
-		// these will run in order.
 		for (Command c : commands) {
 			this.addSequential(c);
 		}
-
-		// To run multiple commands at the same time,
-		// use addParallel()
-		// e.g. addParallel(new Command1());
-		// addSequential(new Command2());
-		// Command1 and Command2 will run in parallel.
-
-		// A command group will require all of the subsystems that each member
-		// would require.
-		// e.g. if Command1 requires chassis, and Command2 requires arm,
-		// a CommandGroup containing them would require both the chassis and the
-		// arm.
 	}
 }
