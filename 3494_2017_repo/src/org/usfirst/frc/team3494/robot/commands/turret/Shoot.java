8ec70633d79b4264ce8dcde80c5919af497222dd
Edwan Vi <tkdberger@gmail.com>
2017-02-25 14:23:35 -0500

    Format source

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/turret/Shoot.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/turret/Shoot.java
@@ -11,38 +11,39 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class Shoot extends Command {
 	private double power;
-	
-    public Shoot() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	this(0.75);
-    }
-    public Shoot(double power) {
-    	super();
-    	this.power = power;
-    	requires(Robot.turret);
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public Shoot() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		this(0.75);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	Robot.turret.shoot(this.power);
-    }
+	public Shoot(double power) {
+		super();
+		this.power = power;
+		requires(Robot.turret);
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return true;
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		Robot.turret.shoot(this.power);
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return true;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
