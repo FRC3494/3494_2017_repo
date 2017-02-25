e480643de2e2f62a10cfee98547bc48ddd16c07a
Edwan Vi <tkdberger@gmail.com>
2017-02-25 12:52:31 -0500

    Add missing @Overrides

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/intake/SwitchPosition.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/intake/SwitchPosition.java
@@ -19,10 +19,12 @@ public class SwitchPosition extends Command {
 	}
 
 	// Called just before this Command runs the first time
+	@Override
 	protected void initialize() {
 	}
 
 	// Called repeatedly when this Command is scheduled to run
+	@Override
 	protected void execute() {
 		if (!Robot.intake.isDeployed) {
 			Robot.intake.pushForward();
@@ -33,16 +35,19 @@ public class SwitchPosition extends Command {
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
+	@Override
 	protected boolean isFinished() {
 		return true;
 	}
 
 	// Called once after isFinished returns true
+	@Override
 	protected void end() {
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
+	@Override
 	protected void interrupted() {
 	}
 }
