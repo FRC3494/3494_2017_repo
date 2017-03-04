183bf0eb6a0e6f088e4b953be40743ba63f9080b
Edwan Vi <tkdberger@gmail.com>
2017-03-04 11:13:28 -0500

    Add missing overrides

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/gears/HoldInState.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/gears/HoldInState.java
@@ -17,26 +17,31 @@ public class HoldInState extends Command {
 	}
 
 	// Called just before this Command runs the first time
+	@Override
 	protected void initialize() {
 	}
 
 	// Called repeatedly when this Command is scheduled to run
+	@Override
 	protected void execute() {
 		Robot.gearTake.setGrasp(Value.kForward);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
+	@Override
 	protected boolean isFinished() {
 		return false;
 	}
 
 	// Called once after isFinished returns true
+	@Override
 	protected void end() {
 		Robot.gearTake.setGrasp(Value.kReverse);
 	}
 
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
+	@Override
 	protected void interrupted() {
 		this.end();
 	}
