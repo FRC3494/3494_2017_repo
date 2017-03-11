28a5a46bb5f57f52e7d4cd26a0db7993f4372bf1
Edwan Vi <tkdberger@gmail.com>
2017-03-10 20:06:54 -0500

    Create LineBreak sensor class

---
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/sensors/LineBreak.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team3494.robot.sensors;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.AnalogTrigger;
+
+public class LineBreak {
+	/**
+	 * Actual analog channel.
+	 */
+	private AnalogInput ai1;
+	/**
+	 * Analog trigger object.
+	 */
+	private AnalogTrigger trigger1;
+	public LineBreak(int channel) {
+		this.ai1 = new AnalogInput(channel);
+		this.trigger1 = new AnalogTrigger(ai1);
+		this.trigger1.setLimitsVoltage(0.777, 3.703);
+	}
+	public LineBreak() {
+		this(1);
+	}
+	/**
+	 * Returns true if the trigger is broken.
+	 */
+	public boolean getBroken() {
+		if (!this.trigger1.getInWindow()) {
+			return trigger1.getTriggerState();
+		} else {
+			return false;
+		}
+	}
+}
