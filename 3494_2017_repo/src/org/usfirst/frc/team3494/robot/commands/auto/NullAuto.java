8e6ee5bde67e2f47a266deedad7eb762e55904aa
Edwan Vi <tkdberger@gmail.com>
2017-03-18 11:49:34 -0400

    Format source

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/NullAuto.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/NullAuto.java
@@ -7,31 +7,36 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class NullAuto extends Command {
 
-    public NullAuto() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    }
+	public NullAuto() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	System.out.println("i am doing nothing");
-    }
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		System.out.println("i am doing nothing");
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
 }
