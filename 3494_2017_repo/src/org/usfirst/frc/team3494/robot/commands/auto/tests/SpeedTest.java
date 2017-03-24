c6d728c8aaa4b790ea0fc55600cf4108162d6255
Edwan Vi <tkdberger@gmail.com>
2017-03-23 20:03:15 -0400

    that's all folks
    http://whatthecommit.com/08bc1a6b0b40e8f149a3a65745a13ef3

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/tests/SpeedTest.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/tests/SpeedTest.java
@@ -6,6 +6,7 @@ import com.ctre.CANTalon;
 import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Test for CAN Talon speed mode.
@@ -19,37 +20,36 @@ public class SpeedTest extends Command {
 
 	// Called just before this Command runs the first time
 	protected void initialize() {
-		for (CANTalon t : Robot.driveTrain.leftSide) {
-			t.changeControlMode(TalonControlMode.Speed);
-			t.setProfile(0);
-			t.setF(0);
-			t.setP(0.1);
-			t.setI(0);
-			t.setD(0);
-		}
-		for (CANTalon t : Robot.driveTrain.rightSide) {
-			t.changeControlMode(TalonControlMode.Speed);
-			t.setProfile(0);
-			t.setF(0);
-			t.setP(0.1);
-			t.setI(0);
-			t.setD(0);
-		}
+		Robot.driveTrain.leftSide[0].changeControlMode(TalonControlMode.Speed);
+		Robot.driveTrain.leftSide[0].setProfile(0);
+		Robot.driveTrain.leftSide[0].setF(0);
+		Robot.driveTrain.leftSide[0].setP(1);
+		Robot.driveTrain.leftSide[0].setI(0);
+		Robot.driveTrain.leftSide[0].setD(0);
+
+		Robot.driveTrain.rightSide[0].changeControlMode(TalonControlMode.Speed);
+		Robot.driveTrain.rightSide[0].setProfile(0);
+		Robot.driveTrain.rightSide[0].setF(0);
+		Robot.driveTrain.rightSide[0].setP(1);
+		Robot.driveTrain.rightSide[0].setI(0);
+		Robot.driveTrain.rightSide[0].setD(0);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-		for (CANTalon t : Robot.driveTrain.leftSide) {
-			t.set(1000);
-		}
-		for (CANTalon t : Robot.driveTrain.rightSide) {
-			t.set(-1000);
-		}
+		SmartDashboard.putNumber("left: ", Robot.driveTrain.leftSide[0].getSpeed());
+		SmartDashboard.putNumber("right:", Robot.driveTrain.rightSide[0].getSpeed());
+		Robot.driveTrain.leftSide[0].set(2000);
+		Robot.driveTrain.leftSide[1].set(0.4);
+		Robot.driveTrain.leftSide[2].set(0.4);
+		Robot.driveTrain.rightSide[0].set(-2500);
+		Robot.driveTrain.rightSide[1].set(-0.4);
+		Robot.driveTrain.rightSide[2].set(-0.4);
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return false; // ten seconds ish
+		return false;
 	}
 
 	// Called once after isFinished returns true
