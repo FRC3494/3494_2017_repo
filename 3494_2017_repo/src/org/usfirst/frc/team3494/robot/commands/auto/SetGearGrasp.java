89f432d9e08fd228b3fa122b9759ea77971f9445
Edwan Vi <tkdberger@gmail.com>
2017-03-24 17:29:56 -0400

    Create SetGearGrasp command

---
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/commands/auto/SetGearGrasp.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team3494.robot.commands.auto;
+
+import org.usfirst.frc.team3494.robot.Robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SetGearGrasp extends Command {
+
+	private Value v;
+	
+    public SetGearGrasp(Value val) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	this.requires(Robot.gearTake);
+    	this.v = val;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.gearTake.setGrasp(v);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
