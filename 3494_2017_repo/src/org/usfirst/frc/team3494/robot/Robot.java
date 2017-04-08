63b434e0214219aaf3f328f7ea33370f4991b683
Edwan Vi <tkdberger@gmail.com>
2017-04-08 12:17:18 -0400

    Remove hack for 3 position gear holder

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/Robot.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/Robot.java
@@ -222,7 +222,6 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
-		Robot.gearTake.setGrasp(Value.kOff);
 		// This makes sure that the autonomous stops running when
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
