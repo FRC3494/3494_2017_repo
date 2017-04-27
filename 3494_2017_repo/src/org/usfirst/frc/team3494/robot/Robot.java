e21d827f4b958fbda3f26f7804309f85a89dc8ab
Edwan Vi <tkdberger@gmail.com>
2017-04-26 20:43:14 -0400

    Remove bad init code

--- 3494_2017_repo/src/org/usfirst/frc/team3494/robot/Robot.java
+++ 3494_2017_repo/src/org/usfirst/frc/team3494/robot/Robot.java
@@ -20,7 +20,6 @@ import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Preferences;
@@ -92,9 +91,6 @@ public class Robot extends IterativeRobot {
 		climber = new Climber();
 		kompressor = new Kompressor();
 		gearTake = new GearTake_2();
-		gearTake.setRamp(true);
-		gearTake.setGrasp(Value.kReverse);
-		gearTake.closeHolder();
 		pto = new ClimberPTO();
 		pto.disengagePTO();
 		// Non subsystem software init
